e8d7c20d75584bff0eecb40012b900fd9b252993
sweakshya60 <shresthasweakshya@gmail.com>
2025-03-10 20:38:15 +0545

    frontend

---
+++ Frontend/src/components/FreeBook.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import list from "../../public/list.json"
+function FreeBook() {
+    const filterData = list.filter((data)=>data.category==="Free");
+    console.log(filterData)
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default FreeBook
